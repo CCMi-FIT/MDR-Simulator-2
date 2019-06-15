a8ed6ea91bb7d8d354742731e75b073d97ef626b
Robert Pergl <perglr@fit.cvut.cz>
2019-06-15 10:01:54 +0200

    deploy update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,7 +2,9 @@
 
 D="~/rsync/mdr-simulator2/"
 
+rm -rf dist/*
 npm run server:build
+rm -rf public/*
 npm run client:build
 rsync -rvpLz --delete -e ssh bin dist node_modules public rob@ccmi.fit.cvut.cz:$D
 rsync -rvpLz --delete -e ssh data rob@ccmi.fit.cvut.cz:$D
