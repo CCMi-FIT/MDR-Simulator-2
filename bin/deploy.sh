df13e0f309be5bf80155b5ee6d8497219d2d5270
Robert Pergl <perglr@fit.cvut.cz>
2019-06-26 17:47:26 +0200

    deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,7 @@ rm -rf dist/*
 npm run server:build
 rm -rf public/*
 npm run client:build
-rsync -rvpLz --delete -e ssh bin dist node_modules public rob@ccmi.fit.cvut.cz:$D
-#rsync -rvpLz --delete -e ssh data rob@ccmi.fit.cvut.cz:$D
-rsync -rvpLz --ignore-existing -e ssh data rob@ccmi.fit.cvut.cz:$D
+rsync -rvLz -e ssh bin dist node_modules public rob@ccmi.fit.cvut.cz:$D
+#rsync -rvLz -e ssh data rob@ccmi.fit.cvut.cz:$D
+rsync -rvLz --ignore-existing -e ssh data rob@ccmi.fit.cvut.cz:$D
 
