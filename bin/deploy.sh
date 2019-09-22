b0765254dc9d3f08a839912104897c028a5e3c2c
Robert Pergl <perglr@fit.cvut.cz>
2019-09-22 11:51:01 +0200

    scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,8 +2,8 @@
 
 D="~/rsync/mdr-simulator2/"
 
-scp -rv bin server/data server/dist server/public package.json rob@ccmi.fit.cvut.cz:$D
-#rsync -rvLz -e ssh bin server node_modules rob@ccmi.fit.cvut.cz:$D
+#scp -rv bin server/data server/dist server/public package.json rob@ccmi.fit.cvut.cz:$D
+rsync -rvLz -e ssh bin server rob@ccmi.fit.cvut.cz:$D
 #rsync -rvLz -e ssh data rob@ccmi.fit.cvut.cz:$D
 #rsync -rvLz --ignore-existing -e ssh data rob@ccmi.fit.cvut.cz:$D
 
--- bin/run.sh
+++ bin/run.sh
@@ -2,5 +2,5 @@
 
 echo "Starting MDR Simulator 2 server..."
 export NODE_ENV="production"
-cd /srv/nodejs/mdr-simulator2/dist
+cd /srv/nodejs/mdr-simulator2/server/dist
 /usr/bin/node server
